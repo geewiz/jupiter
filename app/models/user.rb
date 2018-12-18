560fdca21ea1b5f57b563793bf51ff2c4b56cafb
Jochen Lillich <jochen@lillich.info>
2018-12-18 17:12:13 +0000

    Add annotate rake job

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,29 @@
 # frozen_string_literal: true
-
-# == Schema Information
+# ## Schema Information
 #
-# Table name: users
+# Table name: `users`
 #
-#  id                 :bigint(8)        not null, primary key
-#  email              :string
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  encrypted_password :string(128)
-#  confirmation_token :string(128)
-#  remember_token     :string(128)
-#  first_name         :string(30)
-#  middle_name        :string(30)
-#  last_name          :string(30)
+# ### Columns
+#
+# Name                      | Type               | Attributes
+# ------------------------- | ------------------ | ---------------------------
+# **`id`**                  | `bigint(8)`        | `not null, primary key`
+# **`confirmation_token`**  | `string(128)`      |
+# **`email`**               | `string`           |
+# **`encrypted_password`**  | `string(128)`      |
+# **`first_name`**          | `string(30)`       |
+# **`last_name`**           | `string(30)`       |
+# **`middle_name`**         | `string(30)`       |
+# **`remember_token`**      | `string(128)`      |
+# **`created_at`**          | `datetime`         | `not null`
+# **`updated_at`**          | `datetime`         | `not null`
+#
+# ### Indexes
+#
+# * `index_users_on_email`:
+#     * **`email`**
+# * `index_users_on_remember_token`:
+#     * **`remember_token`**
 #
 
 class User < ApplicationRecord
